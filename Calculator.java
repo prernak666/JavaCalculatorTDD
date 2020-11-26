754f7c4e2a31926e3de5ab1ef41ceb361ca4571d
prernak666 <prernakargil.666@gmail.com>
2020-11-26 19:52:01 +0530

    Adding Test Cases for Zero One and Two Arguments

--- Calculator.java
+++ Calculator.java
@@ -4,13 +4,13 @@ public class Calculator {
   public static int add(String numbers) {
 	int returnValue=0;
 	String[] arr=numbers.split(",");
-	if(arr.length>2)
-		throw new RuntimeException("More than two numbers not allowed");
-	else {
+	//if(arr.length>2)
+		//throw new RuntimeException("More than two numbers not allowed");
+	
 	for(String arr1:arr)
 		if(!arr1.isEmpty()) {
 		returnValue+=Integer.parseInt(arr1);
-		}}
+		}
 	return returnValue;
 }
 }
